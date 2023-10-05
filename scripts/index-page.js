2e361d9919b67551a08893341bf0aea0feec44f3
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-10-05 15:57:08 -0700

    Style comments section bio page at all layouts

---
+++ scripts/index-page.js
@@ -0,0 +1,20 @@
+let defaultComments = [
+  {
+    commentName: "Connor Walton",
+    commentDate: "02/17/2021",
+    commentText:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    commentName: "Emilie Beach",
+    commentDate: "01/09/2021",
+    commentText:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    commentName: "Miles Acosta",
+    commentDate: "12/20/2020",
+    commentText:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
