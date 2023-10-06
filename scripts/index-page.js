02e0a393b1e3ddfa4455d7cf1100155a0d680d44
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-10-05 17:11:16 -0700

    Add comment section with JS, date unformatted. WIP

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -18,3 +18,73 @@ let defaultComments = [
       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
 ];
+
+let commentsSection = document.querySelector(".comments__container");
+let commentForm = document.querySelector(".form");
+
+// console.log(defaultComments);
+
+function appendComments() {
+  commentsSection.textContent = "";
+  for (let i = 0; i < defaultComments.length; i++) {
+    let commentCard = document.createElement("div");
+    commentCard.classList.add("comment");
+
+    let commentCardLeft = document.createElement("div");
+    commentCardLeft.classList.add("comment__left");
+    let commentProfile = document.createElement("div");
+    commentProfile.classList.add("comment__profile");
+
+    let commentCardRight = document.createElement("div");
+    commentCardRight.classList.add("comment__right");
+    let commentCardRightTop = document.createElement("div");
+    commentCardRightTop.classList.add("comment__right-top");
+
+    let commentCardName = document.createElement("p");
+    commentCardName.classList.add("comment__name");
+    commentCardName.innerText = defaultComments[i].commentName;
+
+    let commentCardDate = document.createElement("p");
+    commentCardDate.classList.add("comment__date");
+    commentCardDate.innerText = defaultComments[i].commentDate;
+
+    let commentCardText = document.createElement("p");
+    commentCardText.classList.add("comment__text");
+    commentCardText.innerText = defaultComments[i].commentText;
+
+    commentCard.appendChild(commentCardLeft);
+    commentCard.appendChild(commentCardRight);
+
+    commentCardLeft.appendChild(commentProfile);
+
+    commentCardRight.appendChild(commentCardRightTop);
+    commentCardRight.appendChild(commentCardText);
+
+    commentCardRightTop.appendChild(commentCardName);
+    commentCardRightTop.appendChild(commentCardDate);
+
+    commentsSection.appendChild(commentCard);
+  }
+}
+
+appendComments();
+
+// console.log(commentFormText);
+
+commentForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let newComment = {
+    commentName: event.target.commentFormName.value,
+    commentDate: Date.now(),
+    commentText: event.target.commentFormText.value,
+  };
+
+  console.log(newComment);
+
+  defaultComments.unshift(newComment);
+
+  appendComments();
+
+  commentForm.reset();
+});
