009938c85ea446c911c45966c8fc1aa635b0ba79
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-10-11 21:18:09 -0700

    Update JS files to correctly interface with API

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -34,9 +34,42 @@
 
 const cards = document.querySelector(".cards");
 
-//Add shows array to DOM.
-function appendShows(array) {
-  for (let i = 0; i < array.length; i++) {
+const apiKey = "5f501c9f-d16c-4493-8203-089cc169679b";
+const URL = "https://project-1-api.herokuapp.com/";
+
+class BandSiteAPI {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.baseURL = "https://project-1-api.herokuapp.com/";
+  }
+  async postComment(newComment) {
+    const response = await axios.post(
+      `${this.baseURL}comments?api_key=${apiKey}`,
+      newComment
+    );
+    console.log(response);
+  }
+
+  async getComments() {
+    let response = await axios.get(`${this.baseURL}comments?api_key=${apiKey}`);
+    return response.data;
+  }
+
+  async getShows() {
+    let showInfo = await axios.get(
+      `${this.baseURL}showdates?api_key=${apiKey}`
+    );
+    return showInfo.data;
+  }
+}
+
+console.log("This is connected");
+
+let apiConnection = new BandSiteAPI(apiKey);
+
+async function getShows() {
+  const shows = await apiConnection.getShows();
+  for (let i = 0; i < shows.length; i++) {
     const card = document.createElement("div");
     card.classList.add("card");
 
@@ -69,11 +102,15 @@ function appendShows(array) {
     cardDivider.classList.add("cards__divider");
 
     cardTopTextLight.innerText = "Date";
-    cardTopTextBold.innerText = array[i].date;
+    cardTopTextBold.innerText = new Date(shows[i].date).toDateString();
+    // toLocaleDateString(
+    //   "en-US",
+    //   { timeZone: "UTC" }
+    // );
     cardMiddleTextLight.innerText = "Venue";
-    cardMiddleText.innerText = array[i].venue;
+    cardMiddleText.innerText = shows[i].place;
     cardBottomTextLight.innerText = "Location";
-    cardBottomText.innerText = array[i].location;
+    cardBottomText.innerText = shows[i].location;
     cardButton.innerText = "Buy Tickets";
 
     card.appendChild(cardTop);
@@ -93,21 +130,82 @@ function appendShows(array) {
     cards.appendChild(card);
     cards.appendChild(cardDivider);
   }
+  //Add active state to card on click.
+  let selectedCards = document.querySelectorAll(".card");
+
+  for (let i = 0; i < selectedCards.length; i++) {
+    selectedCards[i].addEventListener("click", function (event) {
+      for (let i = 0; i < selectedCards.length; i++) {
+        if (selectedCards[i] !== event.currentTarget) {
+          selectedCards[i].classList.remove("card--selected"); //Remove active state from all other cards.
+        }
+      }
+
+      event.currentTarget.classList.toggle("card--selected");
+    });
+  }
 }
+getShows();
+
+//Add shows array to DOM.
+// function appendShows(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     const card = document.createElement("div");
+//     card.classList.add("card");
+
+//     const cardTop = document.createElement("div");
+//     cardTop.classList.add("card__top");
+//     const cardTopTextLight = document.createElement("p");
+//     cardTopTextLight.classList.add("card__text", "card__text--light");
+//     const cardTopTextBold = document.createElement("p");
+//     cardTopTextBold.classList.add("card__text", "card__text--bold");
+
+//     const cardMiddle = document.createElement("div");
+//     cardMiddle.classList.add("card__middle");
+//     const cardMiddleTextLight = document.createElement("p");
+//     cardMiddleTextLight.classList.add("card__text", "card__text--light");
+//     const cardMiddleText = document.createElement("p");
+//     cardMiddleText.classList.add("card__text");
+
+//     const cardBottom = document.createElement("div");
+//     cardBottom.classList.add("card__bottom");
+//     const cardBottomTextLight = document.createElement("p");
+//     cardBottomTextLight.classList.add("card__text", "card__text--light");
+//     const cardBottomText = document.createElement("p");
+//     cardBottomText.classList.add("card__text");
+
+//     const cardButton = document.createElement("button");
+//     cardButton.classList.add("card__button");
+//     cardButton.setAttribute("href", "#");
+
+//     const cardDivider = document.createElement("div");
+//     cardDivider.classList.add("cards__divider");
+
+//     cardTopTextLight.innerText = "Date";
+//     cardTopTextBold.innerText = array[i].date;
+//     cardMiddleTextLight.innerText = "Venue";
+//     cardMiddleText.innerText = array[i].venue;
+//     cardBottomTextLight.innerText = "Location";
+//     cardBottomText.innerText = array[i].location;
+//     cardButton.innerText = "Buy Tickets";
+
+//     card.appendChild(cardTop);
+//     card.appendChild(cardMiddle);
+//     card.appendChild(cardBottom);
+//     card.appendChild(cardButton);
+
+//     cardTop.appendChild(cardTopTextLight);
+//     cardTop.appendChild(cardTopTextBold);
+
+//     cardMiddle.appendChild(cardMiddleTextLight);
+//     cardMiddle.appendChild(cardMiddleText);
+
+//     cardBottom.appendChild(cardBottomTextLight);
+//     cardBottom.appendChild(cardBottomText);
+
+//     cards.appendChild(card);
+//     cards.appendChild(cardDivider);
+//   }
+// }
 
 // appendShows(showsArray);
-
-//Add active state to card on click.
-let selectedCards = document.querySelectorAll(".card");
-
-for (let i = 0; i < selectedCards.length; i++) {
-  selectedCards[i].addEventListener("click", function (event) {
-    for (let i = 0; i < selectedCards.length; i++) {
-      if (selectedCards[i] !== event.currentTarget) {
-        selectedCards[i].classList.remove("card--selected"); //Remove active state from all other cards.
-      }
-    }
-
-    event.currentTarget.classList.toggle("card--selected");
-  });
-}
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -22,13 +22,47 @@
 
 let commentsSection = document.querySelector(".comments__container");
 let commentForm = document.querySelector(".form");
+const apiKey = "5f501c9f-d16c-4493-8203-089cc169679b";
+const URL = "https://project-1-api.herokuapp.com/";
 
-//Display comments function, looping through array, including newly posted comment.
-function appendComments() {
+console.log("Connected");
+
+class BandSiteAPI {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.baseURL = "https://project-1-api.herokuapp.com/";
+  }
+  async postComment(newComment) {
+    const response = await axios.post(
+      `${this.baseURL}comments?api_key=${apiKey}`,
+      newComment
+    );
+    console.log(response);
+  }
+
+  async getComments() {
+    let response = await axios.get(`${this.baseURL}comments?api_key=${apiKey}`);
+    response.data.sort((a, b) => {
+      return b.timestamp - a.timestamp;
+    });
+    return response.data;
+  }
+
+  async getShows() {
+    let showInfo = await axios.get(
+      `${this.baseURL}showdates?api_key=${apiKey}`
+    );
+    return showInfo.data;
+  }
+}
+
+let apiConnection = new BandSiteAPI(apiKey);
+
+async function getComments() {
+  const filledComments = await apiConnection.getComments();
   commentsSection.textContent = "";
-  let currentDate = new Date();
 
-  for (let i = 0; i < defaultComments.length; i++) {
+  for (let i = 0; i < filledComments.length; i++) {
     let commentCard = document.createElement("div");
     commentCard.classList.add("comment");
 
@@ -44,7 +78,7 @@ function appendComments() {
 
     let commentCardName = document.createElement("p");
     commentCardName.classList.add("comment__name");
-    commentCardName.innerText = defaultComments[i].commentName;
+    commentCardName.innerText = filledComments[i].name;
 
     let commentCardDate = document.createElement("p");
     commentCardDate.classList.add("comment__date");
@@ -54,7 +88,7 @@ function appendComments() {
      */
 
     const currentDate = new Date();
-    const commentDate = new Date(defaultComments[i].commentDate);
+    const commentDate = new Date(filledComments[i].timestamp);
 
     const timeDifference = currentDate - commentDate;
 
@@ -104,7 +138,7 @@ function appendComments() {
 
     let commentCardText = document.createElement("p");
     commentCardText.classList.add("comment__text");
-    commentCardText.innerText = defaultComments[i].commentText;
+    commentCardText.innerText = filledComments[i].comment;
 
     commentCard.appendChild(commentCardLeft);
     commentCard.appendChild(commentCardRight);
@@ -121,10 +155,10 @@ function appendComments() {
   }
 }
 
-// appendComments();
+getComments();
 
-// Add event listener for form submission.
-commentForm.addEventListener("submit", function (event) {
+//Add event listener for form submission.
+commentForm.addEventListener("submit", async function (event) {
   event.preventDefault();
 
   let nameInput = document.querySelector(".form__name");
@@ -137,29 +171,137 @@ commentForm.addEventListener("submit", function (event) {
     commentText: event.target.commentFormText.value,
   };
 
-  //Form validation to ensure form is properly filled out.
-  if ((newComment.commentName != "") & (newComment.commentText != "")) {
-    // defaultComments.unshift(newComment);
+  console.log(newComment);
+  console.log(event.target.commentFormName.value);
+  console.log(event.target.commentFormText.value);
 
-    appendComments();
-
-    commentForm.reset();
-
-    nameInput.classList.remove("form__name--error");
-    nameInput.placeholder = "Enter Your Name";
-
-    textInput.classList.remove("form__comment--error");
-    textInput.placeholder = "Add a New Comment";
-  } else if ((newComment.commentName == "") & (newComment.commentText == "")) {
-    nameInput.classList.add("form__name--error");
-    nameInput.placeholder = "Please Add Your Name";
-    textInput.classList.add("form__comment--error");
-    textInput.placeholder = "Please Add a Comment";
-  } else if (newComment.commentName == "") {
-    nameInput.classList.add("form__name--error");
-    nameInput.placeholder = "Please Add Your Name";
-  } else if (newComment.commentText == "") {
-    textInput.classList.add("form__comment--error");
-    textInput.placeholder = "Please Add a Comment";
-  }
+  const response = await apiConnection.postComment({
+    name: event.target.commentFormName.value,
+    comment: event.target.commentFormText.value,
+  });
+  getComments();
 });
+
+//Display comments function, looping through array, including newly posted comment.
+// function appendComments() {
+//   commentsSection.textContent = "";
+//   let currentDate = new Date();
+
+//   for (let i = 0; i < defaultComments.length; i++) {
+//     let commentCard = document.createElement("div");
+//     commentCard.classList.add("comment");
+
+//     let commentCardLeft = document.createElement("div");
+//     commentCardLeft.classList.add("comment__left");
+//     let commentProfile = document.createElement("div");
+//     commentProfile.classList.add("comment__profile");
+
+//     let commentCardRight = document.createElement("div");
+//     commentCardRight.classList.add("comment__right");
+//     let commentCardRightTop = document.createElement("div");
+//     commentCardRightTop.classList.add("comment__right-top");
+
+//     let commentCardName = document.createElement("p");
+//     commentCardName.classList.add("comment__name");
+//     commentCardName.innerText = defaultComments[i].commentName;
+
+//     let commentCardDate = document.createElement("p");
+//     commentCardDate.classList.add("comment__date");
+
+//     /*
+//      * Start of dynamic date calculation
+//      */
+
+//     const currentDate = new Date();
+//     const commentDate = new Date(defaultComments[i].commentDate);
+
+//     const timeDifference = currentDate - commentDate;
+
+//     const minutesAgo = Math.floor(timeDifference / (1000 * 60));
+//     const hoursAgo = Math.floor(minutesAgo / 60);
+//     const daysAgo = Math.floor(hoursAgo / 24);
+//     const monthsAgo = Math.floor(daysAgo / 30.437); //average number of days in a month
+//     const yearsAgo = Math.floor(monthsAgo / 12);
+
+//     if (yearsAgo > 0) {
+//       if (yearsAgo === 1) {
+//         commentCardDate.innerText = `${yearsAgo} Year Ago`;
+//       } else {
+//         commentCardDate.innerText = `${yearsAgo} Years Ago`;
+//       }
+//     } else if (monthsAgo > 0) {
+//       if (monthsAgo === 1) {
+//         commentCardDate.innerText = `${monthsAgo} Month Ago`;
+//       } else {
+//         commentCardDate.innerText = `${monthsAgo} Months Ago`;
+//       }
+//     } else if (daysAgo > 0) {
+//       if (daysAgo === 1) {
+//         commentCardDate.innerText = `${daysAgo} Day Ago`;
+//       } else {
+//         commentCardDate.innerText = `${daysAgo} Days Ago`;
+//       }
+//     } else if (hoursAgo > 0) {
+//       if (hoursAgo === 1) {
+//         commentCardDate.innerText = `${hoursAgo} Hour Ago`;
+//       } else {
+//         commentCardDate.innerText = `${hoursAgo} Hours Ago`;
+//       }
+//     } else if (minutesAgo > 0) {
+//       if (minutesAgo === 1) {
+//         commentCardDate.innerText = `${minutesAgo} Minute Ago`;
+//       } else {
+//         commentCardDate.innerText = `${minutesAgo} Minutes Ago`;
+//       }
+//     } else {
+//       commentCardDate.innerText = `Just Now`;
+//     }
+
+//     /*
+//      * End of dynamic date calculation.
+//      */
+
+//     let commentCardText = document.createElement("p");
+//     commentCardText.classList.add("comment__text");
+//     commentCardText.innerText = defaultComments[i].commentText;
+
+//     commentCard.appendChild(commentCardLeft);
+//     commentCard.appendChild(commentCardRight);
+
+//     commentCardLeft.appendChild(commentProfile);
+
+//     commentCardRight.appendChild(commentCardRightTop);
+//     commentCardRight.appendChild(commentCardText);
+
+//     commentCardRightTop.appendChild(commentCardName);
+//     commentCardRightTop.appendChild(commentCardDate);
+
+//     commentsSection.appendChild(commentCard);
+//   }
+// }
+
+// appendComments();
+//Form validation to ensure form is properly filled out.
+//   if ((newComment.commentName != "") & (newComment.commentText != "")) {
+//     getComments();
+
+//     commentForm.reset();
+
+//     nameInput.classList.remove("form__name--error");
+//     nameInput.placeholder = "Enter Your Name";
+
+//     textInput.classList.remove("form__comment--error");
+//     textInput.placeholder = "Add a New Comment";
+//   } else if ((newComment.commentName == "") & (newComment.commentText == "")) {
+//     nameInput.classList.add("form__name--error");
+//     nameInput.placeholder = "Please Add Your Name";
+//     textInput.classList.add("form__comment--error");
+//     textInput.placeholder = "Please Add a Comment";
+//   } else if (newComment.commentName == "") {
+//     nameInput.classList.add("form__name--error");
+//     nameInput.placeholder = "Please Add Your Name";
+//   } else if (newComment.commentText == "") {
+//     textInput.classList.add("form__comment--error");
+//     textInput.placeholder = "Please Add a Comment";
+//   }
+// });
