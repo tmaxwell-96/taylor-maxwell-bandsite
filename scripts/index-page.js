22d78511c536bc11c1cb8559c4ac5a4771430e95
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-10-16 17:11:18 -0700

    Separate displayComments function

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -13,7 +13,117 @@ const URL = "https://project-1-api.herokuapp.com/";
 
 let apiConnection = new BandSiteAPI(apiKey);
 
-function displayComments() {}
+function displayComments(comment) {
+  let commentCard = document.createElement("div");
+  commentCard.classList.add("comment");
+
+  let commentCardLeft = document.createElement("div");
+  commentCardLeft.classList.add("comment__left");
+  let commentProfile = document.createElement("div");
+  commentProfile.classList.add("comment__profile");
+
+  let commentCardRight = document.createElement("div");
+  commentCardRight.classList.add("comment__right");
+  let commentCardRightTop = document.createElement("div");
+  commentCardRightTop.classList.add("comment__right-top");
+
+  let commentCardName = document.createElement("p");
+  commentCardName.classList.add("comment__name");
+  commentCardName.innerText = comment.name;
+
+  let commentCardDate = document.createElement("p");
+  commentCardDate.classList.add("comment__date");
+
+  /*
+   * Start of dynamic date calculation
+   */
+
+  const currentDate = new Date();
+  const commentDate = new Date(comment.timestamp);
+
+  const timeDifference = currentDate - commentDate;
+
+  const minutesAgo = Math.floor(timeDifference / (1000 * 60));
+  const hoursAgo = Math.floor(minutesAgo / 60);
+  const daysAgo = Math.floor(hoursAgo / 24);
+  const monthsAgo = Math.floor(daysAgo / 30.437); //average number of days in a month
+  const yearsAgo = Math.floor(monthsAgo / 12);
+
+  if (yearsAgo > 0) {
+    if (yearsAgo === 1) {
+      commentCardDate.innerText = `${yearsAgo} Year Ago`;
+    } else {
+      commentCardDate.innerText = `${yearsAgo} Years Ago`;
+    }
+  } else if (monthsAgo > 0) {
+    if (monthsAgo === 1) {
+      commentCardDate.innerText = `${monthsAgo} Month Ago`;
+    } else {
+      commentCardDate.innerText = `${monthsAgo} Months Ago`;
+    }
+  } else if (daysAgo > 0) {
+    if (daysAgo === 1) {
+      commentCardDate.innerText = `${daysAgo} Day Ago`;
+    } else {
+      commentCardDate.innerText = `${daysAgo} Days Ago`;
+    }
+  } else if (hoursAgo > 0) {
+    if (hoursAgo === 1) {
+      commentCardDate.innerText = `${hoursAgo} Hour Ago`;
+    } else {
+      commentCardDate.innerText = `${hoursAgo} Hours Ago`;
+    }
+  } else if (minutesAgo > 0) {
+    if (minutesAgo === 1) {
+      commentCardDate.innerText = `${minutesAgo} Minute Ago`;
+    } else {
+      commentCardDate.innerText = `${minutesAgo} Minutes Ago`;
+    }
+  } else {
+    commentCardDate.innerText = `Just Now`;
+  }
+
+  /*
+   * End of dynamic date calculation.
+   */
+
+  let commentCardText = document.createElement("p");
+  commentCardText.classList.add("comment__text");
+  commentCardText.innerText = comment.comment;
+
+  let commentRightBottom = document.createElement("div");
+  commentRightBottom.classList.add("comment__right-bottom");
+
+  let commentLikeButton = document.createElement("button");
+  commentLikeButton.classList.add("comment__like");
+  commentLikeButton.innerText = "Like";
+
+  let commentDeleteButton = document.createElement("button");
+  commentDeleteButton.classList.add("comment__delete");
+  commentDeleteButton.innerText = "Delete";
+
+  let commentLikeCount = document.createElement("p");
+  commentLikeCount.classList.add("comment__like-count");
+  commentLikeCount.innerText = `Likes: ${comment.likes}`;
+
+  commentCard.appendChild(commentCardLeft);
+  commentCard.appendChild(commentCardRight);
+
+  commentCardLeft.appendChild(commentProfile);
+
+  commentCardRight.appendChild(commentCardRightTop);
+  commentCardRight.appendChild(commentCardText);
+  commentCardRight.appendChild(commentRightBottom);
+
+  commentRightBottom.appendChild(commentLikeButton);
+  commentRightBottom.appendChild(commentDeleteButton);
+  commentRightBottom.appendChild(commentLikeCount);
+
+  commentCardRightTop.appendChild(commentCardName);
+  commentCardRightTop.appendChild(commentCardDate);
+
+  commentsSection.appendChild(commentCard);
+}
 
 //Functions
 //-------------------------------------------
@@ -24,115 +134,7 @@ async function getComments() {
     commentsSection.textContent = "";
 
     for (let i = 0; i < filledComments.length; i++) {
-      let commentCard = document.createElement("div");
-      commentCard.classList.add("comment");
-
-      let commentCardLeft = document.createElement("div");
-      commentCardLeft.classList.add("comment__left");
-      let commentProfile = document.createElement("div");
-      commentProfile.classList.add("comment__profile");
-
-      let commentCardRight = document.createElement("div");
-      commentCardRight.classList.add("comment__right");
-      let commentCardRightTop = document.createElement("div");
-      commentCardRightTop.classList.add("comment__right-top");
-
-      let commentCardName = document.createElement("p");
-      commentCardName.classList.add("comment__name");
-      commentCardName.innerText = filledComments[i].name;
-
-      let commentCardDate = document.createElement("p");
-      commentCardDate.classList.add("comment__date");
-
-      /*
-       * Start of dynamic date calculation
-       */
-
-      const currentDate = new Date();
-      const commentDate = new Date(filledComments[i].timestamp);
-
-      const timeDifference = currentDate - commentDate;
-
-      const minutesAgo = Math.floor(timeDifference / (1000 * 60));
-      const hoursAgo = Math.floor(minutesAgo / 60);
-      const daysAgo = Math.floor(hoursAgo / 24);
-      const monthsAgo = Math.floor(daysAgo / 30.437); //average number of days in a month
-      const yearsAgo = Math.floor(monthsAgo / 12);
-
-      if (yearsAgo > 0) {
-        if (yearsAgo === 1) {
-          commentCardDate.innerText = `${yearsAgo} Year Ago`;
-        } else {
-          commentCardDate.innerText = `${yearsAgo} Years Ago`;
-        }
-      } else if (monthsAgo > 0) {
-        if (monthsAgo === 1) {
-          commentCardDate.innerText = `${monthsAgo} Month Ago`;
-        } else {
-          commentCardDate.innerText = `${monthsAgo} Months Ago`;
-        }
-      } else if (daysAgo > 0) {
-        if (daysAgo === 1) {
-          commentCardDate.innerText = `${daysAgo} Day Ago`;
-        } else {
-          commentCardDate.innerText = `${daysAgo} Days Ago`;
-        }
-      } else if (hoursAgo > 0) {
-        if (hoursAgo === 1) {
-          commentCardDate.innerText = `${hoursAgo} Hour Ago`;
-        } else {
-          commentCardDate.innerText = `${hoursAgo} Hours Ago`;
-        }
-      } else if (minutesAgo > 0) {
-        if (minutesAgo === 1) {
-          commentCardDate.innerText = `${minutesAgo} Minute Ago`;
-        } else {
-          commentCardDate.innerText = `${minutesAgo} Minutes Ago`;
-        }
-      } else {
-        commentCardDate.innerText = `Just Now`;
-      }
-
-      /*
-       * End of dynamic date calculation.
-       */
-
-      let commentCardText = document.createElement("p");
-      commentCardText.classList.add("comment__text");
-      commentCardText.innerText = filledComments[i].comment;
-
-      let commentRightBottom = document.createElement("div");
-      commentRightBottom.classList.add("comment__right-bottom");
-
-      let commentLikeButton = document.createElement("button");
-      commentLikeButton.classList.add("comment__like");
-      commentLikeButton.innerText = "Like";
-
-      let commentDeleteButton = document.createElement("button");
-      commentDeleteButton.classList.add("comment__delete");
-      commentDeleteButton.innerText = "Delete";
-
-      let commentLikeCount = document.createElement("p");
-      commentLikeCount.classList.add("comment__like-count");
-      commentLikeCount.innerText = `Likes: ${filledComments[i].likes}`;
-
-      commentCard.appendChild(commentCardLeft);
-      commentCard.appendChild(commentCardRight);
-
-      commentCardLeft.appendChild(commentProfile);
-
-      commentCardRight.appendChild(commentCardRightTop);
-      commentCardRight.appendChild(commentCardText);
-      commentCardRight.appendChild(commentRightBottom);
-
-      commentRightBottom.appendChild(commentLikeButton);
-      commentRightBottom.appendChild(commentDeleteButton);
-      commentRightBottom.appendChild(commentLikeCount);
-
-      commentCardRightTop.appendChild(commentCardName);
-      commentCardRightTop.appendChild(commentCardDate);
-
-      commentsSection.appendChild(commentCard);
+      displayComments(filledComments[i]);
     }
   } catch {
     console.log(
